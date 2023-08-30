6186ddd07d84c5c4965068868003e677a58087ff
Hamza Beder <134306865+HamzaBeder1@users.noreply.github.com>
2023-08-29 21:21:05 -0700

    Highlighting incorrect text, working on context menu

--- SpellChecker/script.js
+++ SpellChecker/script.js
@@ -1,3 +1,88 @@
+let z;
+
+
+window.onload = (event)=>{
+    window.onscroll = function() {scroll()};
+    //window.addEventListener("keydown", moveCursorAtTheEnd);
+    window.addEventListener("contextmenu",showSuggestions);
+    window.addEventListener("keydown", checkSpelling, false);
+    window.addEventListener("contextmenu", DisplaySuggestions);
+    z = new Trie();
+    z.buildTrie();
+}
+
+function scroll(){
+    let header = document.getElementById("header");
+    if(window.pageYOffset > header.offsetTop){
+        header.classList.add("sticky");
+    }
+    else
+    header.classList.remove("sticky");
+} 
+
+function DisplaySuggestions(event){
+    if(event.target.id == "enteredText" && window.getComputedStyle(event.target).textDecorationColor == "rgb(255, 0, 0)"){
+    let menu = document.getElementById("Suggestions");
+
+    let matches = z.findMatches(event.target.textContent);
+    let previousMatches = document.querySelectorAll("matches");
+    previousMatches.forEach(suggestion => suggestion.remove());
+    for(let i = 0; i < matches.length; i++){
+        const newSuggestion = document.createElement("tr");
+        newSuggestion.id = "matches";
+        newSuggestion.style.borderBottom = "solid 1px black"
+        const node = document.createTextNode(`${matches[i]}`);
+        newSuggestion.appendChild(node);
+        menu.appendChild(newSuggestion);
+    }
+    menu.style.position = 'absolute';
+    menu.style.top = event.clientY;
+    menu.style.left = event.clientX;
+    menu.style.display = 'inline';
+
+
+    }
+}
+
+function HideSuggestions(event){
+    document.getElementById("Menu").style.display = 'none';
+}
+
+function moveCursorAtTheEnd(event){
+    var selection=document.getSelection();
+    var range=document.createRange();
+    var contenteditable=document.getElementById('text');
+    if(contenteditable.lastChild.nodeType==3){
+      range.setStart(contenteditable.lastChild,contenteditable.lastChild.length);
+    }else{
+      range.setStart(contenteditable,contenteditable.childNodes.length);
+    }
+    selection.removeAllRanges();
+    selection.addRange(range);
+    }
+
+
+function checkSpelling(event){
+    let el = document.getElementById("text"); 
+    let updatedText = ``;
+    if(event.keyCode == 13){
+        let txt = el.textContent;
+        el.textContent = "";
+        txt = txt.split(" ");
+        for(let i = 0; i < txt.length; i++){
+            if(z.searchWord(txt[i]) == false){
+                el.innerHTML += `<span style="text-decoration:red wavy underline" id = "enteredText">${txt[i] + " "}</span>`;
+            }
+            else{
+                el.innerHTML += `<span>${txt[i] + " "}</span>`;
+            }
+        }
+    }
+}
+
+function showSuggestions(event){
+    
+}
 
 
 class Node{
@@ -38,20 +123,26 @@ class Trie{
     }
 
     buildTrie = function(){
-            const fs = require('fs');
-            const content = fs.readFileSync("../dictionary/dict.txt", 'ascii');
-            let words = content.split("\r\n");
+        fetch('http://localhost:8000/dict')
+        .then(response=>response.text())
+        .then(content=>{
+            console.log("here");
+            let words = content.split(" ");
             for(let i = 0; i < words.length; i++){
                 this.addWord(words[i]);
             }
-        }
+        })
+    }
     
     findMatches = function(target){
+        target = target.replace(/\s+/g,'');
         let currRow = [];
         for(let i = 0; i <= target.length; i++)
             currRow.push(i);
         let matches = [];
         for(let ch in this.root.children){
+            if(ch == 'h')
+                console.log("Stop");
             this.findMatches_aux(target, ch, this.root.children[ch], currRow, matches);
         }
         let i = 0;
@@ -64,6 +155,9 @@ class Trie{
     }
 
     findMatches_aux = function(target, ch, node, prevRow, matches){
+        if(node.word == "hello"){
+            console.log("Stop");
+        }
         let currRow = [prevRow[0]+1];
         for(let i = 1; i <= target.length; i++){
             const insert = prevRow[i] +1;
@@ -87,8 +181,5 @@ class Trie{
 }
 
 
-let z = new Trie();
-console.log("Done");
+z = new Trie();
 z.buildTrie();
-console.log(z.findMatches("once"));
-
